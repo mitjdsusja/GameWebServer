b82d2cc3718d2661da25e3e150e881efbed8ed0d
mitjd <mitjd@SEONG>
2025-10-17 21:44:33 +0900

    feat : Player Info Request - 플레이어 컨트롤러 파일 생성 - 플레이어 컨트롤러 클래스 생성 - 플레이어 정보 요청 응답 함수 작성

---
+++ WebServerProject/Player.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebServerProject.Data;
+
+namespace WebServerProject
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class PlayerController : ControllerBase
+    {
+        private readonly GameDbContext _db;
+        public PlayerController(GameDbContext db) => _db = db;
+
+        [HttpGet("info")]
+        public IActionResult GetPlayerInfo([FromQuery] string userId)
+        {
+            var user = _db.Users.FirstOrDefault(u => u.UserId == userId);
+            if (user == null) return NotFound("User not found");
+
+            return Ok(new
+            {
+                user.UserId,
+                user.Nickname,
+                user.Level,
+                user.Gold,
+                user.Diamonds,
+                user.ProfileId,
+            });
+        }
+    }
+}
