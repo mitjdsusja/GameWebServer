038be8a05b03490b8bb9c4d147d511ea249de814
mitjd <mitjd@SEONG>
2025-10-25 06:39:38 +0900

    refactor : CSR 구조를 위한 파일 생성 및 코드 수정
    각 Service, Repository 파일 생성
    AuthController 코드 수정

---
+++ WebServerProject/Repositories/PlayerRepository.cs
@@ -0,0 +1,26 @@
+﻿using WebServerProject.Data;
+
+namespace WebServerProject.Repositories
+{
+    public class PlayerRepository
+    {
+        private readonly GameDbContext _db;
+
+        public PlayerRepository(GameDbContext db)
+        {
+            _db = db;
+        }
+
+        public User CreateGuestUser()
+        {
+            var user = new User
+            {
+                userId = Guid.NewGuid().ToString(),
+                nickname = "Guest"
+            };
+            _db.users.Add(user);
+            _db.SaveChanges();
+            return user;
+        }
+    }
+}
---
+++ WebServerProject/Services/AuthService.cs
@@ -0,0 +1,33 @@
+﻿using WebServerProject.Data;
+using WebServerProject.Repositories;
+
+namespace WebServerProject.Services
+{
+    public class AuthService
+    {
+        private readonly PlayerRepository _repo;
+
+        public AuthService(PlayerRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public User GuestLogin()
+        {
+            // 필요하다면 추가 검증 로직 가능
+            return _repo.CreateGuestUser();
+        }
+
+        public User GoogleLogin(string email)
+        {
+            // Google 로그인 처리 로직 (생략)
+            throw new NotImplementedException();
+        }
+       
+        public bool CheckUID(string userId)
+        {
+            // UID 확인 로직 (생략)
+            throw new NotImplementedException();
+        }
+    }
+}
