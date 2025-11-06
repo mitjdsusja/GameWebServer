ac623d92831a83dca2f954542458e1184d6bbec3
mitjd <mitjd@SEONG>
2025-11-06 13:34:57 +0900

    feature : characterController 추가

---
+++ WebServerProject/Contollers/CharacterController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebServerProject.Contollers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+    }
+}
