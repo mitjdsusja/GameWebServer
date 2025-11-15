ff121441b16ae3a1e5a6af089c3c086591aadeed
mitjd <mitjdsusja@naver.com>
2025-11-16 05:53:43 +0900

    feature : Deck Entity 작성

--- WebServerProject/Models/Entities/Deck/Deck.cs
+++ WebServerProject/Models/Entities/Deck/Deck.cs
@@ -2,5 +2,9 @@
 {
     public class Deck
     {
+        public int Id { get; set; }
+        public int user_id { get; set; }
+        public string name { get; set; } = "Default Deck";
+        public bool is_active { get; set; } = false;
     }
 }
--- WebServerProject/Models/Entities/Deck/DeckSlot.cs
+++ WebServerProject/Models/Entities/Deck/DeckSlot.cs
@@ -2,5 +2,9 @@
 {
     public class DeckSlot
     {
+        public int id { get; set; }
+        public int deck_id { get; set; }
+        public int user_character_id { get; set; }
+        public byte slot_order { get; set; }    
     }
 }
