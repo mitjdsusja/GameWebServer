ee8986fd4af583be95e2296757221b9ce6fe5a90
mitjd <mitjdsusja@naver.com>
2025-11-15 10:30:03 +0900

    feature : Deck 관련 로직 작성

--- WebServerProject/CSR/Repositories/DeckRepository.cs
+++ WebServerProject/CSR/Repositories/DeckRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace WebServerProject.CSR.Repositories
+﻿using SqlKata.Execution;
+
+namespace WebServerProject.CSR.Repositories
 {
-    public class DeckRepository
+    public interface IDeckRepository
     {
+
+    }
+
+    public class DeckRepository : IDeckRepository
+    {
+        private readonly QueryFactory _db;
+
+        public DeckRepository(QueryFactory db)
+        {
+            _db = db;
+        }
     }
 }
--- WebServerProject/CSR/Services/DeckService.cs
+++ WebServerProject/CSR/Services/DeckService.cs
@@ -1,6 +1,19 @@
-﻿namespace WebServerProject.CSR.Services
+﻿using WebServerProject.CSR.Repositories;
+
+namespace WebServerProject.CSR.Services
 {
-    public class DeckService
+    public interface IDeckService
     {
+
+    }
+    public class DeckService : IDeckService
+    {
+        private readonly IDeckRepository _deckRepository;
+
+        public DeckService(
+            IDeckRepository deckRepository)
+        {
+            _deckRepository = deckRepository;
+        }
     }
 }
