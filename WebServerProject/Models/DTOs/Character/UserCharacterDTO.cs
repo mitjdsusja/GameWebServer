0aa890b635ada0d9891edf215b55a67475b6093d
mitjd <mitjdsusja@naver.com>
2025-12-24 23:45:26 +0900

    fixed : UserCharacterDTO에 temaplteId 멤버 추가

--- WebServerProject/Models/DTOs/Character/UserCharacterDTO.cs
+++ WebServerProject/Models/DTOs/Character/UserCharacterDTO.cs
@@ -6,6 +6,7 @@ namespace WebServerProject.Models.DTOs.Character
     {
         public int userId { get; set; }
         public int userCharacterId { get; set; }
+        public int templateId { get; set; }
         public int level { get; set; } = 1;
         public int experience { get; set; }
         public int stars { get; set; }
@@ -17,6 +18,7 @@ namespace WebServerProject.Models.DTOs.Character
             {
                 userId = character.user_id,
                 userCharacterId = character.id,
+                templateId = character.template_id,
                 level = character.level,
                 experience = character.experience,
                 stars = character.stars,
