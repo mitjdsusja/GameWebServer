50f779c7924ffea373f76d08d785e5e0f731cbc7
mitjd <mitjdsusja@naver.com>
2026-02-24 16:40:41 +0900

    feat : gacha 천장시스템을 위한 sql 수정 파일

--- DB/Init/01_schema.sql
+++ DB/Init/01_schema.sql
@@ -108,7 +108,12 @@ CREATE TABLE gacha_masters (
   cost_amount INT NOT NULL,
   is_limited BOOLEAN NOT NULL,
   start_time DATETIME NOT NULL,
-  end_time DATETIME NOT NULL
+  end_time DATETIME NOT NULL,
+
+  soft_pity_threshold INT NOT NULL DEFAULT 0 COMMENT '점진적 확률 증가 시작 회차',
+  hard_pity_threshold INT NOT NULL DEFAULT 0 COMMENT '100% 확정 지급 회차',
+  pity_bonus_rate DOUBLE NOT NULL DEFAULT 0.0 COMMENT '1스택 당 증가 확률',
+  pity_target_rarity INT NOT NULL DEFAULT 4 COMMENT '천장 대상 희귀도'
 );
 
 -- =========================
@@ -139,6 +144,25 @@ CREATE TABLE gacha_rarity_rates (
     FOREIGN KEY (gacha_id) REFERENCES gacha_masters(id)
     ON DELETE CASCADE
 );
+
+-- =========================
+-- user_gacha_pities
+-- =========================
+CREATE TABLE user_gacha_pities(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  gacha_id INT NOT NULL,
+  pity_stack INT NOT NULL DEFAULT 0,
+  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  CONSTRAINT uq_user_gacha UNIQUE (user_id, gacha_id),
+
+  CONSTRAINT fk_user_gacha_pities_user 
+      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+      
+  CONSTRAINT fk_user_gacha_pities_gacha 
+      FOREIGN KEY (gacha_id) REFERENCES gacha_masters(id) ON DELETE CASCADE
+)
 
 -- =========================
 -- decks
